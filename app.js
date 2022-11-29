0919f89802514d35e7116c5792115657bf3908be
kanibadavi <110975367+kanibadavi@users.noreply.github.com>
2022-11-29 23:44:07 +0330

    first session

---
+++ app.js
@@ -0,0 +1,30 @@
+//values and variables 
+
+
+console.log("kani");
+console.log(23);
+console.log(23+78);
+
+//assigning a value to variable
+//variable variableName = value
+let firstName = "kani";
+console.log(firstName);
+
+/*naming variables rules:
+1-camelCase
+2-do not start with numbers 
+3-they only contatin numbers, letters, underscore(_) and dollar sign
+4-do not use reserved key words
+5-do not start with upper case letter
+6-write a good name for variable
+*/
+let jonas_matilda = "JM";
+
+//correct
+let myFirstJob = "nursing";
+let myCurrentJob = "programming";
+//incorrect
+let job1 = "nursing";
+let job2 = "programming";
+
+console.log(myFirstJob);
